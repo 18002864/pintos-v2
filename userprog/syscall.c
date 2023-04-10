15b5f504fc0c9d9fab465d4bde4e5d053afdaccf
Daniel Sierra <danylang77@gmail.com>
2023-04-10 15:57:09 -0700

    Mas cambios para el syscall se declaran las siguientes funciones sys_halt, sys_exit y sys_write

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -20,3 +20,18 @@ static int get_user(const uint8_t *uaddr);
     UADDR y lo pone en dst, devuelve el numero de bytes leidos o -1 en caso exista page fault
 */
 static int get_user_bytes(void *uaddr, void *dst, size_t bytes);
+/*
+    Termina Pintos llamando a shutdown_power_off () (declarado en devices / shutdown.h).
+    Esto debería usarse pocas veces, porque pierde información sobre posibles situaciones de interbloqueo, etc.
+*/
+void sys_halt(void);
+/*
+    Termina el programa de usuario actual, devolviendo el estado al kernel. Si el padre del proceso
+    lo espera (ver más abajo), este es el estado que se devolverá. Convencionalmente, un estado de 0
+    indica éxito y los valores distintos de cero indican errores.
+*/
+void sys_exit(int);
+/*
+    Escribe size bytes desde el búfer al archivo abierto fd.
+*/
+int sys_write(int fd, const void *buffer, unsigned size);
