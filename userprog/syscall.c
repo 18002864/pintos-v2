aa1d65dc733adf204a2f584bd7a6d2bd674c1590
Daniel Sierra <danylang77@gmail.com>
2023-04-10 15:47:13 -0700

    Primeras implementaciones de los syscall, getUser y getUserBytes

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,18 +3,20 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/process.h"
+#include "devices/shutdown.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-}
-
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
-}
+/*
+    Lee un byte en la dirección virtual del usuario UADDR.
+    UADDR debe estar por debajo de PHYS_BASE.
+    Devuelve el valor del byte si tiene éxito, -1 si ocurrió una falla de segmento.
+*/
+static int get_user(const uint8_t *uaddr);
+/*
+    Lee bytes (bytes) consecutivos, tomando como base la direccion virtual del usuario
+    UADDR y lo pone en dst, devuelve el numero de bytes leidos o -1 en caso exista page fault
+*/
+static int get_user_bytes(void *uaddr, void *dst, size_t bytes);
