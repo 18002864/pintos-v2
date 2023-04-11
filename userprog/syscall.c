ae50e212372359a04059dce9100276f39391e66d
Daniel Sierra <danylang77@gmail.com>
2023-04-10 20:38:49 -0700

    Implementaciones de las funciones declaradas, sys_Exec, sys_write, get user and user bytes

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -45,3 +45,70 @@ void syscall_init(void)
 {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+
+static int get_user(const uint8_t *uaddr)
+{
+  // obtuvimos esta funcion de https://web.stanford.edu/~ouster/cgi-bin/cs140-spring20/pintos/pintos_3.html#SEC44
+  // Accessing User Memory
+  if ((void *)uaddr < PHYS_BASE)
+  {
+    int result;
+    asm("movl $1f, %0; movzbl %1, %0; 1:"
+        : "=&a"(result)
+        : "m"(*uaddr));
+    return result;
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+static int get_user_bytes(void *uaddr, void *dst, size_t bytes)
+{
+  int32_t valor;
+  size_t i; // de acuerdo al size de la estrucutra asi nos vamos moviendo en la memoria
+  for (i = 0; i < bytes; i++)
+  {
+    valor = get_user(uaddr + i); // usamos get user para leer el byte
+    if (valor == -1)
+    {
+      return -1;
+    }
+    else
+    {
+      *(char *)(dst + i) = valor & 0xff; // solo dejamos pasar el byte hacia dst
+    }
+  }
+};
+
+int sys_write(int fd, const void *buffer, unsigned size)
+{
+  // validamos que no este accesando a memoria que no debe
+  if (get_user((const uint8_t *)buffer) == -1)
+  {
+    thread_exit();
+    return 0;
+  }
+  // Todos nuestros programas de prueba escriben en la consola
+  if (fd == 1)
+  {
+    putbuf(buffer, size);
+    return size;
+  }
+  return 0;
+};
+
+tid_t sys_exec(const char *cmd_line)
+{
+
+  // cmd_line es un puntero a donde esta el argumento, debemos verificar que sea
+  // valida la direccion
+  if (get_user((const uint8_t *)cmd_line) == -1)
+  {
+    thread_exit();
+    return -1;
+  }
+
+  return process_execute(cmd_line);
+}
